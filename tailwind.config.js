5ef077c0e945d296de16295124a48a69bb476bb5
Newton Ombese <newtonombese1@gmail.com>
2023-04-02 16:38:56 +0300

    :art::lipstick:style & feat: Adding some color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#667085',
+        buttonColor: '#16B1B5',
+      },
+    },
   },
   plugins: [],
 }
