4ab3a3fae63e084684e16c981abd9fefc8680345
kelvin <carreuky>
2023-04-04 21:01:44 +0300

    🎨 changes on font height

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,13 +7,13 @@ export default function Homepage() {
       <div className="text-2xl">
         Integrated Complaints Referral Mechanism At County Level
       </div>
-      <div className="w-3/4 text-[20px] leading-7 ">
+      <div className="w-3/4 text-[20px]  ">
         Quality service and accountability from government and non governmental
         service providers is your right. TI Kenya and its partners will ensure
         your message is forwarded to the relevant aid or service provider and
         ensure that you are given a feedback.
       </div>
-      <div className="text-[24px] leading-4">
+      <div className="text-[22px]">
         Send your complain via <span className="text-[#16b1b5] font-medium">SMS</span> to
         the number <span className="text-[#16b1b5] font-medium ">7777</span>
       </div>
