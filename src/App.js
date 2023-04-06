343da035b40b8237de9a227d645edd3548225ae2
Newton Ombese <newtonombese1@gmail.com>
2023-04-06 23:56:22 +0300

    :art:feat: Adding contact info component to the app js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Homepage from "./components/Homepage";
 import Initiative from "./components/Initiative";
+import ContactInfo from "./components/ContactInfo";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar />
       <Homepage />
       <Initiative />
+      <ContactInfo />
     </>
   );
 }
